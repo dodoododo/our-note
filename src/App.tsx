c13613036abdc24a2fb7da701813b131c273a4d4
dodoododo <109458037+dodoododo@users.noreply.github.com>
2025-12-22 20:01:22 +0700

    Initialize Project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button.tsx";
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <button className="bg-green-400">Click me</button>
+    </>
+  )
+}
+
+export default App
